127e201f708aaef91e792fa47d93282631f2ec6f
Nkwe <nkabo074@gmail.com>
2024-03-14 13:38:36 +0100

    add file support

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,12 +1,64 @@
 /**
+ * Cipher a text contain within a file (.txt in that case) 
+ * @param {*} htmlId HTML id of the button
+ * @param {*} cipherDecision Change the behevior of the function: cipher if true else decipher
+ */
+function applyCipherOrDecipherOnFile(htmlId, cipherDecision) 
+{
+  let button = document.getElementById(htmlId);
+
+  button.onclick = function () {
+    let fileInput = document.getElementById("file-input");
+    let file = fileInput.files[0];
+
+    let fileReader = new FileReader();
+
+    fileReader.onload = function (event) {
+      let fileContent = event.target.result;
+      let fileContent_toString = fileContent.toString();
+
+      let decalage = parseInt(
+        document.getElementById("Decalage-screen").value,
+        10
+      );
+
+      let encryptedContent = ceasarCipher(
+        fileContent_toString,
+        decalage,
+        cipherDecision
+      );
+
+      // Create a new Blob with the encrypted content
+      let encryptedBlob = new Blob([encryptedContent], { type: "text/plain" });
+
+      // Create a temporary URL for the encrypted Blob
+      let encryptedURL = URL.createObjectURL(encryptedBlob);
+
+      // Create a temporary anchor element to trigger the download
+      let downloadLink = document.createElement("a");
+      downloadLink.href = encryptedURL;
+      downloadLink.download = "encrypted.txt";
+      downloadLink.click();
+
+      // Clean up the temporary resources
+      URL.revokeObjectURL(encryptedURL);
+    };
+
+    fileReader.readAsText(file);
+  };
+}
+
+/**
  * Cipher or decipher a message using the "Ceasar cipher"
  * @param {*} message Text you want to cipher or decipher
  * @param {*} k Cipher key
  * @param {*} isCipher Change the behevior of the function: cipher if true else decipher
  * @returns The ciphered or deciphred message
  */
-function ceasarCipher(message, k, isCipher) {
-  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?";
+function ceasarCipher(message, k, isCipher) 
+{
+  const alphabet =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?";
   let temp = message.split("");
 
   for (let i = 0; i < message.length; i++) {
@@ -32,7 +84,8 @@ function ceasarCipher(message, k, isCipher) {
  * @param {*} htmLid HTML id of the button
  * @param {*} cipherDecision Change the behevior of the function: cipher if true else decipher
  */
-function applyCipherOrDecipher(htmLid, cipherDecision) {
+function applyCipherOrDecipher(htmLid, cipherDecision) 
+{
   let result = document.getElementById(htmLid);
 
   result.onclick = function () {
@@ -51,3 +104,6 @@ function applyCipherOrDecipher(htmLid, cipherDecision) {
 }
 applyCipherOrDecipher("sendToCipher", true);
 applyCipherOrDecipher("sendToDecipher", false);
+
+applyCipherOrDecipherOnFile("encrypt-button", true);
+applyCipherOrDecipherOnFile("decrypt-button", false);
